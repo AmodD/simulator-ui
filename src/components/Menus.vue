5059db1557edd2402f6609ecaa8362dc967f9b40
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-11-01 12:27:57 +0530

    TV-169 #comment adding log out

--- src/components/Menus.vue
+++ src/components/Menus.vue
@@ -2,12 +2,19 @@
 	<div>
 		<nav class="navbar is-dark">
 		  <!-- navbar brand, navbar menu... -->
+		      <div class="navbar-end">
+		      <div id="logout" class="navbar-item">
+			<a @click="storeAction('logout')">
+				<span class="icon has-text-danger"><i class="fas fa-sign-out-alt"></i></span>
+			</a>
+		      </div>
+	     	      </div>	      
 		</nav>
 	<div class="section">	
 	<div class="column is-1">
 	    <aside class="menu is-pulled-left">
 		<p class="menu-label">
-			<a @click="createurl('access_home')">
+			<a @click="storeAction('access_home')">
 				<div class="box has-background-dark tooltip" data-tooltip="Dashboard" >
 					<span class="icon has-text-white"><i class="fas fa-2x fa-home"></i></span>
 				</div>
@@ -15,7 +22,7 @@
 			<hr>
 		</p>
 		<p class="menu-label">
-			<a @click="createurl('access_care')">
+			<a @click="storeAction('access_care')">
 				<div class="box has-background-dark tooltip" data-tooltip="CARE" >
 					<span class="icon has-text-white"><i class="fas fa-2x fa-suitcase"></i></span>
 				</div>
@@ -23,7 +30,7 @@
 			<hr>
 		</p>
 		<p class="menu-label">
-			<a @click="createurl('access_alarm')">
+			<a @click="storeAction('access_alarm')">
 				<div class="box has-background-dark tooltip" data-tooltip="ALARM" >
 					<span class="icon has-text-white"><i class="fas fa-2x fa-users"></i></span>
 				</div>
@@ -31,7 +38,7 @@
 			<hr>
 		</p>
 		<p class="menu-label">
-			<a @click="createurl('access_aim')">
+			<a @click="storeAction('access_aim')">
 				<div class="box has-background-dark tooltip" data-tooltip="AIM" >
 					<span class="icon has-text-white"><i class="fas fa-2x fa-code-branch"></i></span>
 				</div>
@@ -39,7 +46,7 @@
 			<hr>
 		</p>
 		<p class="menu-label">
-			<a @click="createurl('access_sats')">
+			<a @click="storeAction('access_sats')">
 				<div class="box has-background-dark tooltip" data-tooltip="SATS">
 					<span class="icon has-text-white"><i class="fas fa-2x fa-atom"></i></span>
 				</div>
@@ -47,7 +54,7 @@
 			<hr>
 		</p>
 		<p class="menu-label">
-			<a @click="createurl('access_metl')">
+			<a @click="storeAction('access_metl')">
 				<div class="box has-background-dark tooltip" data-tooltip="METL">
 					<span class="icon has-text-white"><i class="fab fa-2x fa-hubspot"></i></span>
 				</div>
@@ -79,9 +86,9 @@ export default {
     };
   },
   methods: {
-    createurl: function(module) {
+    storeAction: function(action) {
       this.$store
-        .dispatch(module)
+        .dispatch(action)
 	.then(() => { })
 	.catch(err => console.log(err));
     }
